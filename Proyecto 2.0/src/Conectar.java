9854e5d9ea7c2534fe61e5cf1ed042e5ff094307
diazMafer <31084398+diazMafer@users.noreply.github.com>
2017-11-09 22:11:19 -0600

    jueves

--- Proyecto 2.0/src/Conectar.java
+++ Proyecto 2.0/src/Conectar.java
@@ -8,17 +8,7 @@ import javax.swing.JOptionPane;
 import org.mongodb.morphia.query.UpdateOperations;
 
 
-
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-    
+ 
 
 public class Conectar{
     private Datastore ds;
